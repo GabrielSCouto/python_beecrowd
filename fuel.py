05f6b38ebb65f51846a6d8553cd6aaf8d3ea614e
gabrielcouto <goldevil2015@gmail.com>
2025-01-19 23:55:52 -0300

    add fuel / gasto de combustivel (1017)

---
+++ fuel.py
@@ -0,0 +1,11 @@
+#FUEL (GASTO DE COMBUSTIVEL) - SUBMISSION #43125597
+
+def main():
+
+    time = int(input())
+    avg_speed = int(input())
+
+    a = (avg_speed * time) / 12
+
+    print(f'{a:.3f}')
+main()    
