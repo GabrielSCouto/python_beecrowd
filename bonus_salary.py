fec88a78c484ba0fc75b7e29e472a711b3943710
gabrielcouto <goldevil2015@gmail.com>
2025-01-20 22:18:10 -0300

    add bonus salary / salario com bonus (1009)

---
+++ bonus_salary.py
@@ -0,0 +1,9 @@
+#BONUS SALARY (SALÁRIO COM BÔNUS) - SUBMISSION #43134161
+
+def main():
+    name = input()
+    salary = float(input())
+    sales = float(input())
+
+    print(f'TOTAL = R$ {salary + sales * 0.15:.2f}')
+main()    
